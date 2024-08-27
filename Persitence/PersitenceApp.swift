46f1058019b04b3314b4777de01b69c67f68854b
Jonas <jonas.gregory@outlook.com>
2024-08-27 09:23:35 -0500

    Initial Commit

---
+++ Persitence/PersitenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersitenceApp.swift
+//  Persitence
+//
+//  Created by Jonas R. Gregory on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct PersitenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

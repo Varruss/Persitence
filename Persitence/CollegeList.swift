b59b1ae989c3d57e52354bb1aeecf9fe9cd8f1b4
Jonas <jonas.gregory@outlook.com>
2024-09-09 08:19:38 -0500

    l

---
+++ Persitence/CollegeList.swift
@@ -0,0 +1,18 @@
+//
+//  CollegeList.swift
+//  Persitence
+//
+//  Created by Jonas R. Gregory on 9/5/24.
+//
+
+import SwiftUI
+
+struct CollegeList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CollegeList()
+}
